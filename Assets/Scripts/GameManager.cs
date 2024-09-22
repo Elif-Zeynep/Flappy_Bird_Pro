947adff9803e0deab9dcd3a6fa88bb95526d7096
Elif-Zeynep <63517839+Elif-Zeynep@users.noreply.github.com>
2024-09-22 19:19:38 +0300

    highscore & pause funcionality added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,32 +11,58 @@ public class GameManager : MonoBehaviour
     public GameObject playButton;
     public GameObject gameOverText;
 
-    private int score;
+    private int _score, _bestScore;
+    private bool _isPaused = false;
+
+    [SerializeField]
+    private Text _pauseText, _resumeText, _bestScoreText;
 
     public void Awake()
     {
         Application.targetFrameRate = 60;
-        Pause();
+        StopGame();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _bestScore = PlayerPrefs.GetInt("HighScore", 0);
+        _bestScoreText.text = "Best: " + _bestScore;
+        _pauseText.gameObject.SetActive(false);
+        _bestScoreText.gameObject.SetActive(false);
+        _resumeText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (_isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                Pause();
+            }
+        }
     }
 
     public void Play()
     {
-        score = 0;
-        scoreText.text = score.ToString();
+        _score = 0;
+        scoreText.text = _score.ToString();
         playButton.SetActive(false);
         gameOverText.SetActive(false);
+        _pauseText.gameObject.SetActive(false);
+        _bestScoreText.gameObject.SetActive(false);
+        _resumeText.gameObject.SetActive(false);
+        StartCoroutine(PlayPauseText());
 
         Time.timeScale = 1f;
         player.enabled = true;
@@ -56,22 +82,59 @@ public class GameManager : MonoBehaviour
 
     public void Pause()
     {
+        _isPaused = true;
+        Time.timeScale = 0f;
+        //player.enabled = false;
+        _resumeText.gameObject.SetActive(true);
+    }
+
+    public void StopGame()
+    {
         Time.timeScale = 0f;
         player.enabled = false;
     }
 
     public void GameOver()
     {
-        //Debug.Log("Game Over");
+        UpdateBestScore();
+        _bestScoreText.gameObject.SetActive(true);
         gameOverText.SetActive(true);
         playButton.SetActive(true);
-        Pause();
+        StopGame();
     }
 
     public void IncreaseScoreByOne()
     {
-        //Debug.Log("Score");
-        score++;
-        scoreText.text = score.ToString();
+        _score++;
+        scoreText.text = _score.ToString();
+    }
+
+    public void ResumeGame()
+    {
+        _resumeText.gameObject.SetActive(false);
+        _isPaused = false;
+        Time.timeScale = 1;
+    }
+
+    public void MakePaused()
+    {
+        _isPaused = false;
+        _resumeText.gameObject.SetActive(true);
+    }
+
+    public void UpdateBestScore()
+    {
+        if (_score > _bestScore)
+        {
+            _bestScore = _score;
+            PlayerPrefs.SetInt("HighScore", _bestScore);
+        }
+    }
+
+    IEnumerator PlayPauseText()
+    {
+        _pauseText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(4f);
+        _pauseText.gameObject.SetActive(false);
     }
 }
